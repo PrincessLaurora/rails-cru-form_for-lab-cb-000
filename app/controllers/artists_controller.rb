6cfefb360649435d457632162c00339a0e8ca950
Laura Felce <crepu83@yahoo.it>
2019-05-29 09:45:15 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -24,7 +24,7 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id])
-    @artist.update(artist_params(:bio))
+    @artist.update(artist_params(:name))
     redirect_to artist_path(@artist)
   end
 end
